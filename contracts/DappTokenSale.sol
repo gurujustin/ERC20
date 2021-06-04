12b78ac58b1373e76cdbf94ad5751d586c1d1ebc
stepanBokhanov <kkh0102www@outlook.com>
2021-06-04 16:10:28 +1000

    #64

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -6,6 +6,9 @@ contract DappTokenSale {
     address admin;
     DappToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokenSold;
+    
+    event Sell(address _buyer, uint256 _amount);
 
     constructor (DappToken _tokenContract, uint256 _tokenPrice) public {
         // Assign an admin
@@ -14,5 +17,23 @@ contract DappTokenSale {
         tokenContract = _tokenContract;
         // Token Price
         tokenPrice = _tokenPrice;
+    }
+
+    // multiply
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        // Require that value is equal to tokens
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        // Require that the contract has enough tokens
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        // Require that a transfer is successful
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        // Keep track of tokenSold
+        tokenSold += _numberOfTokens;
+        // Trigger sell event
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -1,8 +1,14 @@
 const DappTokenSale = artifacts.require("DappTokenSale");
+const DappToken = artifacts.require("DappToken");
 
 contract('DappTokenSale', function(accounts) {
     var tokenSaleInstance;
+    var tokenInstance;
     var tokenPrice = 1000000000000000;
+    var buyer = accounts[1];
+    var admin = accounts[0];
+    var numberOfTokens;
+    var tokensAvailable = 750000;
 
     it('initializes the contracts with the correct values', function() {
         return DappTokenSale.deployed().then(function(instance) {
@@ -18,4 +24,35 @@ contract('DappTokenSale', function(accounts) {
             assert.equal(price, tokenPrice, 'token price is correct');
         });
     });
+
+    it('facillitates token buying', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return DappTokenSale.deployed();
+        }).then(function(instance) {
+            tokenSaleInstance = instance;
+            tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
+        }).then(function(receipt){
+            numberOfTokens = 10;
+            var value = numberOfTokens * tokenPrice;
+            return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: value})
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the sell event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            return tokenSaleInstance.tokenSold();
+        }).then(function(amount) {
+            assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+            return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: 1 });
+        }).then(assert.fail).catch(function(err) {
+            assert(err.message.indexOf('revert') >= 0, 'err.message must contain revert');
+            return tokenSaleInstance.buyTokens(800000, {from: buyer, value: numberOfTokens * tokenPrice });
+        }).then(assert.fail).catch(function(err) {
+            assert(err.message.indexOf('revert') >= 0, 'balace should be bigger than balance');
+        });
+    })
 });
