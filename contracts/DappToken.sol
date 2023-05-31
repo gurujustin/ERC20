3f6bb3cdd7dee43224e6e4e38c39a87a0bfa09e1
Guru Justin <67410422+gurujustin@users.noreply.github.com>
2023-06-01 01:15:56 +1000

    Update DappToken.sol

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -14,13 +14,13 @@ contract DappToken {
     mapping(address => uint256) public balanceOf;
     //  allowance
     mapping(address => mapping(address => uint256)) public allowance;
-
+    // Transfer Event
     event Transfer(
         address indexed _from,
         address indexed _to,
         uint256 _value
     );
-
+    // Approve Event
     event Approval(
         address indexed _owner,
         address indexed _spender,
